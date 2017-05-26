74fb5747304203a0105f43f4efee9b367e066632
PrachiGupta <prachi_gupta10@infosys.com>
2017-05-26 11:23:21 +0530

    Dev2 changed line7 11:23

--- HelloJavaGit/src/com/infy/HelloWorldClass.java
+++ HelloJavaGit/src/com/infy/HelloWorldClass.java
@@ -4,6 +4,7 @@ public class HelloWorldClass {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("Line1");
+		System.out.println("Dev1 Line2");
 	}
 
 }
