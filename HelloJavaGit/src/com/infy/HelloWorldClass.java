f9b851a00c51c452c91037c62a48de9e0146432d
PrachiGupta <prachi_gupta10@infosys.com>
2017-05-26 11:28:49 +0530

    Dev2 changed Line5 11:28

--- HelloJavaGit/src/com/infy/HelloWorldClass.java
+++ HelloJavaGit/src/com/infy/HelloWorldClass.java
@@ -2,9 +2,9 @@ package com.infy;
 
 public class HelloWorldClass {
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.println("Hello World from Dev2");
 		System.out.println("Line1");
-		System.out.print("Dev2 Line2");
+		
 	}
 
 }
